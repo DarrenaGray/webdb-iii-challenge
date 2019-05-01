7edf81bcf38ac7dd97cf640db16985d071c43f04
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-01 18:53:24 -0400

    Created endpoint GET for retrieving students in database. Currently returns and empty array

--- data/server.js
+++ data/server.js
@@ -97,4 +97,16 @@ server.delete('/api/cohorts/:id', (req, res) => {
         });
 });
 
+// Students
+server.get('/api/students', (req, res) => {
+    db('students')
+        .then(students => {
+            res.status(200).json(students);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+
 module.exports = server;
